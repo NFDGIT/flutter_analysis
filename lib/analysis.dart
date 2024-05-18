34823b864e69007567382105aeef2e62594c7a12
penghui <penghui@renruihr.com>
2024-05-18 11:45:26 +0800

    删除插件内的平台判断

--- lib/analysis.dart
+++ lib/analysis.dart
@@ -4,16 +4,10 @@ import 'analysis_platform_interface.dart';
 class Analysis {
   /// 获取iOS归因token
   Future<String?> getIOSAttributionToken()async {
-    if (!Platform.isIOS) {
-      return null;
-    }
     return await AnalysisPlatform.instance.getIOSAttributionToken();
   }
   /// 获取设备id
   Future<String?> getDeviceId() async{
-    if (!Platform.isIOS) {
-      return null;
-    }
     return await AnalysisPlatform.instance.getDeviceId();
   }
 }
